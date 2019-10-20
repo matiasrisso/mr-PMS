08727228a63ba49eb73a87fee7ccdff77bec86fe
idan bm <idanbe5@gmail.com>
2019-10-20 15:37:39 +0200

    CORS

--- app.py
+++ app.py
@@ -1,21 +1,15 @@
 from environs import Env
 from flask import Flask
-# from flask_swagger_ui import get_swaggerui_blueprint
 from db.models import db
 from db.seed import *
 from routes import routes_blueprint
+from flask_cors import CORS
 
 
 # Swagger config
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.yaml'
-# SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
-#     SWAGGER_URL,
-#     API_URL,
-#     config={
-#         'app_name': "Best-PMS-ever"
-#     }
-# )
+
 
 # Load pre defined environment variables
 env = Env()
@@ -23,6 +17,7 @@ env.read_env()
 
 # Flask sqlalchemy configurations
 app = Flask(__name__, instance_relative_config=True)
+CORS(app)
 if env("ENV") == "dev":
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = env("SQLALCHEMY_LOCAL_DATABASE_URI")
@@ -43,7 +38,17 @@ seed_all()
 
 # Connect routes to app
 app.register_blueprint(routes_blueprint)
-# app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
+if env("ENV") == "dev":
+    from flask_swagger_ui import get_swaggerui_blueprint
+    SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+        SWAGGER_URL,
+        API_URL,
+        config={
+            'app_name': "Best-PMS-ever"
+        }
+    )
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 if __name__ == '__main__':
     # Run the app
